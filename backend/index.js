8741227a52abf2be2c0994881f5daf94cdffd768
abdulaziz-esmail <72497863+abdulaziz-esmail@users.noreply.github.com>
2025-08-13 03:22:29 +0200

    git all tasks from database

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,16 @@ const PORT = 5000
 app.use(cors())
 app.use(bodyParser.json())
 
+//fetch all tasks
+app.get('/tasks',(req, res) => {
+    db.query('SELECT * FROM tasks', (err, results) => {
+        if (err) return res.status(500).json(err)
+        //res.send("<h1>hi</h1>")
+        res.json(results)
+    })
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost: ${PORT}`)
+})
